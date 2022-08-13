31e2ca63a1568ba1834af142a9384c6ac127258c
school_pi <tantalum_zhao@qq.com>
2022-08-13 10:54:26 -0500

    完善Python版

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as gpio, time, sys
 
-'''
+
 gpio.setmode(gpio.BOARD)
 #使用PWM类定义EN使能端,PWM频率为1kHz
 gpio.setup(40, gpio.OUT)
 fans = gpio.PWM(40,1000)
 #占空比为0,确保风扇停转
 fans.start(0)
-'''
-file = open("/sys/class/thermal/thermal_zone0/temp")
-# 读取结果，并转换为浮点数
-#print(file.read())
-temp = float(file.read()) / 1000
-# 关闭文件
-file.close()
+
+#while True:
+try:
+    with open("/sys/class/thermal/thermal_zone0/temp",'r') as file:
+        # 读取结果，并转换为浮点数
+        #print(file.read())
+        temp = float(file.read()) / 1000
+    #print(temp)
+    # 35-75+ -> 0-100
+    if temp == 0:
+        raise Exception('温度文件异常')
+    speed = int((temp - low)/(high-low)*100) if temp<high and temp >low else 0 if temp<=low else 1
+    fans.ChangeDutyCycle(speed)
+except :
+    with open('./log','a') as file:
+        file.writelines([time.ctime(),'\n',sys.exc_info()[0],'\n'])
+    break
+
+gpio.cleanup()
 
 #print("temp="+str(temp))
