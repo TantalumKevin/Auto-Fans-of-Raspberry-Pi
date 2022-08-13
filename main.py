1d70298190dba66b29935e2d38eb7977fff77554
school_pi <tantalum_zhao@qq.com>
2022-08-13 11:34:32 -0500

    改了改py

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ gpio.setup(40, gpio.OUT)
 fans = gpio.PWM(40,1000)
 #占空比为0,确保风扇停转
 fans.start(0)
+low = 35
+high = 75
+min = 5
 
-#while True:
-try:
-    with open("/sys/class/thermal/thermal_zone0/temp",'r') as file:
-        # 读取结果，并转换为浮点数
-        #print(file.read())
-        temp = float(file.read()) / 1000
-    #print(temp)
-    # 35-75+ -> 0-100
-    if temp == 0:
-        raise Exception('温度文件异常')
-    speed = int((temp - low)/(high-low)*100) if temp<high and temp >low else 0 if temp<=low else 1
-    fans.ChangeDutyCycle(speed)
-except :
-    with open('./log','a') as file:
-        file.writelines([time.ctime(),'\n',sys.exc_info()[0],'\n'])
-    break
+while True:
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp",'r') as file:
+            # 读取结果，并转换为浮点数
+            #print(file.read())
+            temp = float(file.read()) / 1000
+        #print(temp)
+        # 35-75+ -> 0-100
+        if temp == 0:
+            raise Exception('温度文件异常')
+        speed = int((temp - low)/(high-low)*100) if temp<high and temp >low else 0 if temp<=low else 1
+        fans.ChangeDutyCycle(speed)
+    except :
+        with open('./log','a') as file:
+            file.writelines([time.ctime(),'\n',sys.exc_info()[0],'\n'])
+        break
+    time.sleep(min*60)
 
 gpio.cleanup()
 
